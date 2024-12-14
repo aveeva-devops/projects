d42bb01cc5c0ae364b9793dad15bf44c443ce634
devOps Master <aveeva.devops@gmail.com>
2024-12-14 16:48:45 -0500

    changes done for terraform install guide

---
+++ Unix/testscript.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# GNU bash, version 4.4.20
+
+# A simple Bash script to greet the user
+
+# Display a greeting message
+echo "Hello! Welcome to this simple Bash script."
+
+# Ask for the user's name
+read -p "What is your name? " name
+
+# Greet the user personally
+echo "Nice to meet you, $name!"
+
+# End the script
+echo "Have a great day!"
